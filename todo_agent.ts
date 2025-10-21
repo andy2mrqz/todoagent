9a737217c50291be60f6991d586104812970b4fa
Andrew Marquez <andy2mgcc@gmail.com>
2025-10-21 14:11:38 -0600

    Connect agent to the todo api

--- todo_agent.ts
+++ todo_agent.ts
@@ -2,7 +2,9 @@ import { Agent } from './agent.ts'
 import { type AgentTool } from './tool.ts'
 
 export class TodoAgent extends Agent {
-  constructor() {
+  protected todoApi: string
+
+  constructor(todoApi: string = 'http://localhost:3000') {
     super(`
       You are a TodoAgent that helps the user manage their Todo list.
 
@@ -10,18 +12,22 @@ export class TodoAgent extends Agent {
 
       Do not offer to help with things you do not have a tool for.
     `)
-    this.tools = [listTodosTool]
+    this.todoApi = todoApi
+    this.tools = [this.listTodosTool]
   }
-}
 
-// Tools
-const listTodosTool: AgentTool = {
-  name: 'list_todos',
-  description: "You list the user's todos",
-  input_schema: {
-    type: 'object',
-  },
-  fn: async () => {
-    return '\n1. Take out the trash\n2. Get some milk'
-  },
+  // Tools
+
+  listTodosTool: AgentTool = {
+    name: 'list_todos',
+    description: "You list the user's todos",
+    input_schema: {
+      type: 'object',
+    },
+    fn: async () => {
+      const res = await fetch(`${this.todoApi}/api/todos`)
+      const body = await res.text()
+      return body
+    },
+  }
 }
